dabaaa4599e9f66c3267e9bb3f6414983b8247c6
alwandp <alwandp34@gmail.com>
2022-09-25 12:50:58 +0700

    tugas 7 - suara hewan menggunakan jquery

---
+++ pertemuan-7/js/script.js
@@ -0,0 +1,172 @@
+$(document).ready(function () {
+    // Hewan unggas
+    $("div.bebek").click(function () {
+        $(this).css({
+            background: "url('assets/images/bebek.png')",
+            color: "transparent"
+        });
+    });
+    $("div.bebek").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/bebek.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.bebek");
+    });
+
+    $("div.ayam").click(function () {
+        $(this).css({
+            background: "url('assets/images/ayam.png')",
+            color: "transparent"
+        });
+    });
+    $("div.ayam").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/ayam.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.ayam");
+    });
+    
+    $("div.angsa").click(function () {
+        $(this).css({
+            background: "url('assets/images/angsa.png')",
+            color: "transparent"
+        });
+    });
+    $("div.angsa").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/angsa.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.angsa");
+    });
+
+    $("div.burung").click(function () {
+        $(this).css({
+            background: "url('assets/images/burung.png')",
+            color: "transparent"
+        });
+    });
+    $("div.burung").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/burung.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.burung");
+    });
+
+    // Hewan ternak
+    $("div.domba").click(function () {
+        $(this).css({
+            background: "url('assets/images/domba.png')",
+            color: "transparent"
+        });
+    });
+    $("div.domba").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/domba.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.domba");
+    });
+
+    $("div.sapi").click(function () {
+        $(this).css({
+            background: "url('assets/images/sapi.png')",
+            color: "transparent"
+        });
+    });
+    $("div.sapi").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/sapi.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.sapi");
+    });
+
+    $("div.unta").click(function () {
+        $(this).css({
+            background: "url('assets/images/unta.png')",
+            color: "transparent"
+        });
+    });
+    $("div.unta").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/unta.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.unta");
+    });
+
+    $("div.kuda").click(function () {
+        $(this).css({
+            background: "url('assets/images/kuda.png')",
+            color: "transparent"
+        });
+    });
+    $("div.kuda").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/kuda.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.kuda");
+    });
+
+    // Hewan buas
+    $("div.singa").click(function () {
+        $(this).css({
+            background: "url('assets/images/singa.png')",
+            color: "transparent"
+        });
+    });
+    $("div.singa").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/singa.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.singa");
+    });
+
+    $("div.harimau").click(function () {
+        $(this).css({
+            background: "url('assets/images/harimau.png')",
+            color: "transparent"
+        });
+    });
+    $("div.harimau").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/harimau.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.harimau");
+    });
+
+    $("div.serigala").click(function () {
+        $(this).css({
+            background: "url('assets/images/serigala.png')",
+            color: "transparent"
+        });
+    });
+    $("div.serigala").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/serigala.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.serigala");
+    });
+
+    $("div.buaya").click(function () {
+        $(this).css({
+            background: "url('assets/images/buaya.png')",
+            color: "transparent"
+        });
+    });
+    $("div.buaya").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'assets/audio/buaya.mp3',
+            'volume': 1,
+            'autoplay': 'autoplay'
+        }).appendTo("div.buaya");
+    });
+  });
